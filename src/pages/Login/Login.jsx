e09428f18ae57fb0e7d5fdffd9b762ef64b818ff
shakia-ripa <shakiaripa@gmail.com>
2023-10-09 22:47:42 +0600

    added login route

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Login;
