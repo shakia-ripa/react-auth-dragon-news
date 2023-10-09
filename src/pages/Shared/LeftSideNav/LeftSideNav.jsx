a230a0ab814a95139929a635360441a8960c3764
shakia-ripa <shakiaripa@gmail.com>
2023-10-09 22:24:33 +0600

    loaded nav links on left side nav

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h2 className="text-2xl">Left Side</h2>
+            <h2 className="text-2xl font-semibold">All Category</h2>
+            {
+                categories.map(category => <Link className="block" 
+                key={category.id}
+                to={`/category/${category.id}`}><button className="btn btn-ghost text-xl font-medium">{category.name}</button></Link>)
+            }
         </div>
     );
 };
